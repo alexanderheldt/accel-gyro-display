f85b53ca33a5dd7a150d50c15732fb8108dfaca0
Alexander Heldt <me@alexanderheldt.se>
2021-10-20 21:18:04 +0200

    Toggle pin with XOR

--- main.c
+++ main.c
@@ -5,10 +5,10 @@ int main() {
   DDRB = (1 << DDB2);
 
   while(1) {
-    PORTB = (1 << PB2);
     _delay_ms(2000);
+    PORTB ^= (1 << PB2);
 
-    PORTB = (0 << PB2);
+    PORTB ^= (1 << PB2);
     _delay_ms(1000);
   }
 
